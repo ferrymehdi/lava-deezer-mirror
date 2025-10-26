0c810d46fc80df29f6bd41bb43a4363b46ef7362
ferrymehdi <mehdiferry996@gmail.com>
2025-10-26 15:49:20 +0100

    chore: update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,14 @@ pluginManagement {
         kotlin("jvm") version "1.9.21"
     }
 }
+
 rootProject.name = "lava-deezer-mirror"
+
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
